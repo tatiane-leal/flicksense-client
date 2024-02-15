273a37e03d13c55eeb12fb55640a63db4d6cad5a
Tatiane Sala <tatianedleal@gmail.com>
2024-02-15 01:13:47 -0500

    update interfaces

--- src/models/movie.interface.ts
+++ src/models/movie.interface.ts
@@ -21,3 +21,8 @@ export interface Movie {
   vote_average: number;
   vote_count: number;
 }
+
+export interface MoviePayload {
+  id: string | undefined;
+  movies: Movie[];
+}
--- src/models/user.interface.ts
+++ src/models/user.interface.ts
@@ -1,4 +1,20 @@
+import { JwtPayload } from 'jwt-decode';
+
 export interface User {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
+}
+
+export interface CurrentUser {
+  asObservable(): import('rxjs').Observable<CurrentUser | null>;
+  UserInfo: UserInfo;
+  exp: number;
+  iat: number;
+}
+
+export interface UserInfo {
+  name: string;
+  id: string;
+  email: string;
+  roles: string[];
 }
