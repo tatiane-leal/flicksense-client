8cc84e905aa47e084acf99fd9a013d83d606ddd9
Tatiane Sala <tatianedleal@gmail.com>
2024-02-15 01:13:27 -0500

    update services

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from '../../models/post.interface';
+import { Movie, MoviePayload } from '../../models/movie.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -23,5 +24,10 @@ export class ApiService {
   getUserProfile(userId: string | null): Observable<any> {
     const url = `http://localhost:3000/profile/${userId}`;
     return this._http.get(url);
+  }
+
+  saveMovies(payload: MoviePayload): Observable<any> {
+    const url = `http://localhost:3000/users`;
+    return this._http.put(url, payload);
   }
 }
--- src/app/services/token-storage.service.ts
+++ src/app/services/token-storage.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { CurrentUser } from '../../models/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenStorageService {
   private readonly TOKEN_KEY = 'accessToken';
+  private readonly CURRENT_USER_KEY = 'currentUser';
 
   constructor() {}
 
@@ -16,7 +18,17 @@ export class TokenStorageService {
     return sessionStorage.getItem(this.TOKEN_KEY);
   }
 
+  public saveDecodedToken(currentUser: CurrentUser): void {
+    sessionStorage.setItem(this.CURRENT_USER_KEY, JSON.stringify(currentUser));
+  }
+
+  public getDecodedToken(): CurrentUser | null {
+    const currentUser = sessionStorage.getItem(this.CURRENT_USER_KEY);
+    return currentUser ? JSON.parse(currentUser) : null;
+  }
+
   public clearToken(): void {
     sessionStorage.removeItem(this.TOKEN_KEY);
+    sessionStorage.removeItem(this.CURRENT_USER_KEY);
   }
 }
