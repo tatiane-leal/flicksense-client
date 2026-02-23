074b484c8ff7cebabf4b334964c29d7c40c0f4db
Tatiane Sala <tatianedleal@gmail.com>
2026-02-23 15:20:39 -0500

    Update movie.interface.ts

--- src/models/movie.interface.ts
+++ src/models/movie.interface.ts
@@ -1,6 +1,36 @@
-export interface PhysicalAI {
+original
+
+export interface MovieApiResponse {
   page: number;
   results: Movie[];
   total_pages: number;
   total_results: number;
+}
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  sentiment_result: {
+    isHappy?: boolean;
+    isNeutral?: boolean;
+    isSad?: boolean;
+  };
+}
+
+export interface MoviePayload {
+  user?: any;
+  id: string | undefined;
+  movies: Movie[];
 }
